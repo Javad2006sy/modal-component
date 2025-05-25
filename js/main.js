2c6c683300c39184f5dc390c3e496d13577ce668
Mr_ J <javad.seyyedi20006@gmail.com>
2025-05-25 14:55:59 +0330

    Added keyboard functionality for closing modals

--- js/main.js
+++ js/main.js
@@ -2,17 +2,28 @@ const modal = document.querySelector('#modal');
 const modalOpenBtn = document.querySelector('#modal-open');
 const modalCloseBtn = modal.querySelector('#modal-close');
 
+let activeModal = null;
+
 // Adding event listeners to modal elements
 modalOpenBtn.addEventListener('click', () => {openModal(modal);});
 modalCloseBtn.addEventListener('click', () => {closeModal(modal);});
 modal.querySelector('.modal-overlay').addEventListener('click', () => {closeModal(modal)});
 
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && activeModal) {
+        e.preventDefault();
+        closeModal(activeModal);
+    };
+});
+
 // Function for opening modal
 function openModal(modalElement) {
     modalElement.setAttribute('data-hidden', false);
+    activeModal = modalElement;
 }
 
 // Function for closing modal
 function closeModal(modalElement) {
     modalElement.setAttribute('data-hidden', true);
+    activeModal = null;
 }
